bf1a955cab35e72e34ed65d5709f25035126e8df
mikeja008 <mikeja008@gmail.com>
2017-04-09 02:10:16 -0400

    basic elements for design

--- app/src/main/java/com/css/cssapp/ChatScreen.java
+++ app/src/main/java/com/css/cssapp/ChatScreen.java
@@ -6,24 +6,39 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.sparkpost.SparkPost;
+import com.sparkpost.exception.SparkPostException;
 
 public class ChatScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button bitellButton;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        bitellButton = (Button)findViewById(R.id.speechbutton);
+
+
+        bitellButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent swipe;
+                swipe= new Intent(ChatScreen.this, DashboardLeader.class);
+                startActivity(swipe);
             }
         });
+
     }
 
 }
--- app/src/main/java/com/css/cssapp/DashboardLeader.java
+++ app/src/main/java/com/css/cssapp/DashboardLeader.java
@@ -6,25 +6,38 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.sparkpost.SparkPost;
+import com.sparkpost.exception.SparkPostException;
 
 public class DashboardLeader extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button speechButton;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_leader);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        speechButton = (Button)findViewById(R.id.speechbutton);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        speechButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent chat = new Intent(DashboardLeader.this, ChatScreen.class);
+                startActivity(chat);
             }
         });
+
     }
 
 }
