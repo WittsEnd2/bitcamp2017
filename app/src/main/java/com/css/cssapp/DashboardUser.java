1851b1761df34e5fc35fda185bea7fa1dbf240c0
Michael Wittner <Michael Wittner>
2017-04-08 23:49:33 -0400

    Database Retrieval for User

--- app/src/main/java/com/css/cssapp/DashboardUser.java
+++ app/src/main/java/com/css/cssapp/DashboardUser.java
@@ -5,19 +5,51 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
+import static com.google.android.gms.internal.zzs.TAG;
+
+
 public class DashboardUser extends AppCompatActivity {
-
-
+    ArrayList<Projects> projectList = new ArrayList<Projects>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_user);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("projects");
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    Projects post = postSnapshot.getValue(Projects.class);
+                    projectList.add(post);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("The read failed: " ,databaseError.getMessage());
+            }
+        });
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +60,8 @@ public class DashboardUser extends AppCompatActivity {
             }
         });
     }
+
+
 
 
 }
---
+++ app/src/main/java/com/css/cssapp/Projects.java
@@ -0,0 +1,56 @@
+package com.css.cssapp;
+
+/**
+ * Created by micha on 4/8/2017.
+ */
+
+public class Projects {
+    private String projectLeader;
+    private String projectName;
+    private String projectDescription;
+    private int projectId;
+    private int numPeople;
+    public Projects() {
+
+    }
+
+    public String getProjectLeader() {
+        return projectLeader;
+    }
+
+    public void setProjectLeader(String projectLeader) {
+        this.projectLeader = projectLeader;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public int getNumPeople() {
+        return numPeople;
+    }
+
+    public void setNumPeople(int numPeople) {
+        this.numPeople = numPeople;
+    }
+}
